a06fc239157e59845e69d4a7eafc7a83a98f2de2
Edgar Klepek <edgarklepek@gmail.com>
2022-04-20 08:02:55 +0200

    35. Adding Basic CSS Styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
-function ExpenseItem () {
-    return <h2>Expense item!</h2>
+import './ExpenseItem.css';
+
+function ExpenseItem() {
+    return (
+        <div className="expense-item">
+            <div>17.07.2021</div>
+            <div className="expense-item__description">
+                <h2>Autoversicherung</h2>
+                <div className="expense-item__price">294,50€</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
