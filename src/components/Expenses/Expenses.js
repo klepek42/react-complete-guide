9a48dda8ad8f37e3eca8d29a529ea399a8065af2
Edgar Klepek <edgarklepek@gmail.com>
2022-05-03 15:41:49 +0200

    67. Outputting Conditional Content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,6 +16,19 @@ const Expenses = (props) => {
         (expense) => expense.date.getFullYear().toString() === selectedFilter
     );
 
+    let expensesContent = <p>No expenses found.</p>;
+
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => (
+            <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+            />
+        ));
+    }
+
     return (
         <div>
             <Card className="expenses">
@@ -23,14 +36,7 @@ const Expenses = (props) => {
                     selected={selectedFilter}
                     onChangeFilter={filterChangeHandler}
                 />
-                {filteredExpenses.map((expense) => (
-                    <ExpenseItem
-                        key={expense.id}
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}
-                    />
-                ))}
+                {expensesContent}
             </Card>
         </div>
     );
