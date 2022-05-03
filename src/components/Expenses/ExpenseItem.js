f025c05c8837a43abb76feadc6484e5222bed1ea
Edgar Klepek <edgarklepek@gmail.com>
2022-05-03 07:58:24 +0200

    61. Controlled vs Uncontrolled Components & Stateless vs Statefull Components

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-    // Hook
-    const [title, setTitle] = useState(props.title); // Set default value
-    console.log('ExpenseItem evaluated by React');
-
-    const clickHandler = () => {
-        setTitle('Updated!');
-        console.log(title);
-    };
-
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className="expense-item__price">{props.amount}€</div>
             </div>
-            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 };
