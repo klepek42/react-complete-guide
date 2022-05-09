22ae200d9b6dc7592ae35e8b48529d9406e70562
Edgar Klepek <edgarklepek@gmail.com>
2022-05-09 13:51:11 +0200

    71. Wrap Up & Next Steps

--- src/App.js
+++ src/App.js
@@ -1,56 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import NewExpense from './components/NewExpense/NewExpense';
-import Expenses from './components/Expenses/Expenses';
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
 
 const DUMMY_EXPENSES = [
-    {
-        id: 'e1',
-        title: 'Toilet Paper',
-        amount: 94.12,
-        date: new Date(2020, 7, 14),
-    },
-    {
-        id: 'e2',
-        title: 'New TV',
-        amount: 799.49,
-        date: new Date(2021, 2, 12),
-    },
-    {
-        id: 'e3',
-        title: 'Car Insurance',
-        amount: 294.67,
-        date: new Date(2021, 2, 28),
-    },
-    {
-        id: 'e4',
-        title: 'New Desk (Wooden)',
-        amount: 450,
-        date: new Date(2021, 5, 12),
-    },
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
 ];
 
 const App = () => {
-    const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
-    const addExpenseHandler = (expense) => {
-        setExpenses((prevExpenses) => {
-            return [expense, ...prevExpenses];
-        });
-    };
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
 
-    const cancelExpenseHandler = () => {
-        console.log('cancelExpenseHandler');
-        return;
-    };
-
-    // JSX
-    return (
-        <div>
-            <NewExpense onAddExpense={addExpenseHandler} />
-            <Expenses items={expenses} />
-        </div>
-    );
+  // JSX
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
+  );
 };
 
 export default App;
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,32 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Card from '../UI/Card';
-import ExpensesFilter from '../ExpensesFilter/ExpensesFilter';
-import ExpensesList from './ExpensesList';
-import './Expenses.css';
+import Card from "../UI/Card";
+import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+import "./Expenses.css";
 
 const Expenses = (props) => {
-    const [selectedFilter, setSelectedFilter] = useState('2021');
+  const [selectedFilter, setSelectedFilter] = useState("2021");
 
-    const filterChangeHandler = (filterData) => {
-        setSelectedFilter(filterData);
-    };
+  const filterChangeHandler = (filterData) => {
+    setSelectedFilter(filterData);
+  };
 
-    const filteredExpenses = props.items.filter(
-        (expense) => expense.date.getFullYear().toString() === selectedFilter
-    );
+  const filteredExpenses = props.items.filter(
+    (expense) => expense.date.getFullYear().toString() === selectedFilter
+  );
 
-    return (
-        <div>
-            <Card className="expenses">
-                <ExpensesFilter
-                    selected={selectedFilter}
-                    onChangeFilter={filterChangeHandler}
-                />
-                <ExpensesList items={filteredExpenses} />
-            </Card>
-        </div>
-    );
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={selectedFilter}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
 };
 
 export default Expenses;
