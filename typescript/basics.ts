098dc401a82622222ecbfa5149668fffabff9cc6
klepek42 <edgarklepek@gmail.com>
2022-06-09 11:01:49 +0200

    399. Exploring the Base Types

---
+++ typescript/basics.ts
@@ -0,0 +1,12 @@
+// Primitives: number, strings, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+let age: number = 24;
+
+let userName: string;
+userName = "Max";
+
+let isInstructor: boolean;
+isInstructor = true;
