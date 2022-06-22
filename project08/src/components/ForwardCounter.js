ff0ccbe9dfc62846f177c92d6b58ac025958ca8f
klepek42 <edgarklepek@gmail.com>
2022-06-22 08:18:42 +0200

    190. Configuring Custom Hooks

--- project08/src/components/ForwardCounter.js
+++ project08/src/components/ForwardCounter.js
@@ -1,10 +1,9 @@
-import { useState, useEffect } from "react";
-
 import Card from "./Card";
 import useCounter from "../hooks/use-counter";
 
 const ForwardCounter = () => {
   const counter = useCounter();
+  console.log("counter", counter);
 
   return <Card>{counter}</Card>;
 };
