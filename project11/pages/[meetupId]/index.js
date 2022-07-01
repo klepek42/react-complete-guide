9f6f6bef5b4fe32c80ad7136256018e775d72658
klepek42 <edgarklepek@gmail.com>
2022-07-01 13:43:57 +0200

    334. Adding Custom Components & CSS Modules

---
+++ project11/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://cms.finnair.com/resource/blob/721284/0511bb2bc91953d4ef3506d00933819b/tokyo-main-data.jpg"
+      title="Rainbow Meetup"
+      address="Shibuya 109, Tokyo"
+      description="Let's have a lot of fun in the center of Japan."
+    />
+  );
+}
+
+export default MeetupDetails;
