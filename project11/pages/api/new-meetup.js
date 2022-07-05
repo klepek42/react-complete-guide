da995882f9472822cc1b26d44f7d4e8065dd987d
klepek42 <edgarklepek@gmail.com>
2022-07-05 10:27:22 +0200

    342. Working with MongoDB

--- project11/pages/api/new-meetup.js
+++ project11/pages/api/new-meetup.js
@@ -1,11 +1,27 @@
+import { MongoClient } from "mongodb";
+
 // /api/new-meetup
 // endpoint for creating a new meetup
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
     const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://reactcomplete:QXFI7W0D6QFuvVEp@cluster0.bdepqdo.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log("result", result);
+
+    await client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
 
