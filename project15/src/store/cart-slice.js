74fb07982ab2b96e4c46e85626b4859274889885
klepek42 <edgarklepek@gmail.com>
2022-07-14 13:48:15 +0200

    253. Refresher / Practice: Part 2/2

--- project15/src/store/cart-slice.js
+++ project15/src/store/cart-slice.js
@@ -10,10 +10,12 @@ const cartSlice = createSlice({
   reducers: {
     addItemToCart(state, action) {
       const newItem = action.payload;
-      const existingItem = state.item.find((item) => item.id === newItem.id);
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
+
       if (!existingItem) {
-        state.item.push({
-          itemId: newItem.id,
+        state.items.push({
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
@@ -24,6 +26,21 @@ const cartSlice = createSlice({
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
-    removeItemFromCart() {},
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
   },
 });
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
