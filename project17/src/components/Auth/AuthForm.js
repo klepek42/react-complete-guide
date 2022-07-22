d6b171534050fc3997aab52843b492feb451af55
klepek42 <edgarklepek@gmail.com>
2022-07-22 13:45:47 +0200

    306. Adding User Login

--- project17/src/components/Auth/AuthForm.js
+++ project17/src/components/Auth/AuthForm.js
@@ -22,28 +22,31 @@ const AuthForm = () => {
     // optional: Add validation
 
     setIsLoading(true);
+    let url;
+
     if (isLogin) {
-      // fetch(
-      //   // "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA61PCrcP6IqcsJGyh7CPhHYzlwwD_G248"
-      // );
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA61PCrcP6IqcsJGyh7CPhHYzlwwD_G248";
     } else {
-      fetch(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA61PCrcP6IqcsJGyh7CPhHYzlwwD_G248",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      ).then((res) => {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA61PCrcP6IqcsJGyh7CPhHYzlwwD_G248";
+    }
+
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
         setIsLoading(false);
         if (res.ok) {
-          // ...
+          return res.json();
         } else {
           res.json().then((data) => {
             // show an error modal
@@ -52,11 +55,16 @@ const AuthForm = () => {
             // if (data && data.error && data.error.message) {
             //   errorMessage = data.error.message;
             // }
-            alert(errorMessage);
+            throw new Error(errorMessage);
           });
         }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        alert(err.message);
       });
-    }
   };
 
   return (
