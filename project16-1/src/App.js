33905a0c7f5fb140445addc72474689124535aac
klepek42 <edgarklepek@gmail.com>
2022-09-09 14:34:43 +0200

    280. Adding Dummy Data & More Content

--- project16-1/src/App.js
+++ project16-1/src/App.js
@@ -2,24 +2,27 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import AllQuotes from "./components/pages/AllQuotes";
 import NewQuote from "./components/pages/NewQuote";
 import QuoteDetail from "./components/pages/QuoteDetail";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
     <div>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/quotes" />
-        </Route>
-        <Route path="/quotes" exact>
-          <AllQuotes />
-        </Route>
-        <Route path="/quotes/:quoteId">
-          <QuoteDetail />
-        </Route>
-        <Route path="/new-quote">
-          <NewQuote />
-        </Route>
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" exact>
+            <AllQuotes />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail />
+          </Route>
+          <Route path="/new-quote">
+            <NewQuote />
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
