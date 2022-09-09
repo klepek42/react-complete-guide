416dc4f4148be502dce118548363e5e9d1eb1f5e
klepek42 <edgarklepek@gmail.com>
2022-09-09 12:01:02 +0200

    276. Redirecting The User

--- project16/src/App.js
+++ project16/src/App.js
@@ -1,6 +1,6 @@
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import MainHeader from "./components/MainHeader";
 import ProductDetail from "./pages/ProductDetail";
 
@@ -10,6 +10,9 @@ function App() {
       <MainHeader />
       <main>
         <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
           <Route path="/welcome">
             <Welcome />
           </Route>
