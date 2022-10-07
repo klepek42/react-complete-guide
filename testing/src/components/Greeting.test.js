e494a6a37ac3d0bcb6780670d9d8fcdf6f8b0d45
klepek42 <edgarklepek@gmail.com>
2022-10-07 21:52:00 +0200

    391. Testing User Interaction & State

--- testing/src/components/Greeting.test.js
+++ testing/src/components/Greeting.test.js
@@ -1,5 +1,6 @@
 import Greeting from "./Greeting";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 describe("Greeting component", () => {
   test("renders Hello World as a text", () => {
@@ -12,5 +13,37 @@ describe("Greeting component", () => {
     // Assert
     const helloWorldElement = screen.getByText("Hello World!");
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("render 'It's good to see you!' initialy", () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Assert
+    expect(screen.getByText("It's good to see you!")).toBeDefined();
+  });
+
+  test("render 'Changed!' when the button was clicked", () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const button = screen.getByRole("button", { name: "Changed text!" });
+    userEvent.click(button);
+
+    // Assert
+    expect(screen.getByText("Changed!")).toBeDefined();
+  });
+
+  test("don't render 'It's good to see you!' when the button was clicked", () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const button = screen.getByRole("button", { name: "Changed text!" });
+    userEvent.click(button);
+
+    // Assert
+    expect(screen.queryByText("good to see you!", { exact: false })).toBeNull();
   });
 });
