4bd934903e53eedfd186ccb9d4e3095f82a41a62
klepek42 <edgarklepek@gmail.com>
2022-10-28 13:41:08 +0200

    288. Writing More Flexible Routing Code

--- project16-1/src/components/pages/QuoteDetail.js
+++ project16-1/src/components/pages/QuoteDetail.js
@@ -1,4 +1,4 @@
-import { Link, Route, useParams } from "react-router-dom";
+import { Link, Route, useParams, useRouteMatch } from "react-router-dom";
 import Comments from "../comments/Comments";
 import HighlightedQuote from "../quotes/HighlightedQuote";
 
@@ -8,7 +8,10 @@ const DUMMY_QUOTES = [
 ];
 
 const QuoteDetail = () => {
+  const match = useRouteMatch();
+  console.log("match", match);
   const params = useParams();
+
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
   if (!quote) {
@@ -18,14 +21,14 @@ const QuoteDetail = () => {
   return (
     <section>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route path={`/quotes/${params.quoteId}`} exact>
+      <Route path={match.path} exact>
         <div className="centered">
-          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+          <Link className="btn--flat" to={`${match.url}/comments`}>
             Load Comments
           </Link>
         </div>
       </Route>
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </section>
