b5f08de9c7d646f4973577d455122c6759be5544
klepek42 <edgarklepek@gmail.com>
2022-11-11 16:04:34 +0100

    292. Better Data Fetching with React Router 6.4 - First example of easier data fetching

--- project16-4/src/App.jsx
+++ project16-4/src/App.jsx
@@ -1,27 +1,40 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
 import BlogLayout from "./pages/BlogLayout.jsx";
-import BlogPostsPage from "./pages/BlogPosts.jsx";
+import BlogPostsPage, {
+  loader as blogPostsLoader,
+} from "./pages/BlogPosts.jsx";
 import NewPostPage from "./pages/NewPost.jsx";
-import PostDetailPage from "./pages/PostDetail.jsx";
-import RootLayout from "./components/RootLayout.jsx";
+import PostDetailPage, {
+  loader as blogPostLoader,
+} from "./pages/PostDetail.jsx";
 import WelcomePage from "./pages/Welcome.jsx";
+import RootLayout from "./pages/RootLayout.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<WelcomePage />} />
+      <Route path="/blog" element={<BlogLayout />}>
+        <Route index element={<BlogPostsPage />} loader={blogPostsLoader} />
+        <Route
+          path=":id"
+          element={<PostDetailPage />}
+          loader={blogPostLoader}
+        />
+      </Route>
+      <Route path="/blog/new" element={<NewPostPage />} />
+    </Route>
+  )
+);
 
 function App() {
-  return (
-    <BrowserRouter>
-      <RootLayout>
-        <Routes>
-          <Route path="/" element={<WelcomePage />} />
-          <Route path="/blog" element={<BlogLayout />}>
-            <Route index element={<BlogPostsPage />} />
-            <Route path=":id" element={<PostDetailPage />} />
-          </Route>
-          <Route path="/blog/new" element={<NewPostPage />} />
-        </Routes>
-      </RootLayout>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
