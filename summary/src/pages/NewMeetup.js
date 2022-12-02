c5aa6c85564b57fdf75032b45b191f22b2a85e63
klepek42 <edgarklepek@gmail.com>
2022-12-02 14:08:23 +0100

    479. Navigating Programmatically

--- summary/src/pages/NewMeetup.js
+++ summary/src/pages/NewMeetup.js
@@ -1,6 +1,9 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import { useHistory } from "react-router-dom";
 
 function NewMeetup() {
+  const history = useHistory();
+
   const addMeetupHandler = (meetupData) => {
     fetch(
       "https://react-summary-f3b83-default-rtdb.europe-west1.firebasedatabase.app/meetup.json",
@@ -9,7 +12,7 @@ function NewMeetup() {
         body: JSON.stringify(meetupData),
         header: { "Content-Type": "application/json" },
       }
-    );
+    ).then(() => history.replace("/"));
   };
 
   return (
